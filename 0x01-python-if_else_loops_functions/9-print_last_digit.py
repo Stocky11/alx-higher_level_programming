53ab3e6967a085cce961b3cbc38cd0f66c2b3020
Stocky11 <joycestocky96@gmail.com>
2023-07-12 19:59:56 -0700

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,7 @@
 
 
 def print_last_digit(number):
-"""Print the last digit of a number and return it."""
+ """Print the last digit of a number and return it."""
 print(abs(number) % 10, end="")
-return (abs(number) % 10)
+ return (abs(number) % 10)
 
